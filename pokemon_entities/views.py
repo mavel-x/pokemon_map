614c50ac32b4c77ab6c3ba971d4c12b55d568821
mavel <p.m.berdnikov@gmail.com>
2022-08-20 14:41:33 +0300

    show pokemons on main page map

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -28,23 +28,22 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
+    # with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
+    #     pokemons = json.load(database)['pokemons']
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
-            )
+    for entity in PokemonEntity.objects.all():
+        add_pokemon(
+            folium_map, entity.latitude,
+            entity.longitude,
+            request.build_absolute_uri(entity.pokemon.image.url) if entity.pokemon.image else None,
+        )
 
     pokemons_on_page = []
     for pokemon in Pokemon.objects.all():
         pokemons_on_page.append({
             'pokemon_id': pokemon.id,
-            'img_url': request.build_absolute_uri(pokemon.image.url) if pokemon.image else '',
+            'img_url': request.build_absolute_uri(pokemon.image.url) if pokemon.image else None,
             'title_ru': pokemon.title,
         })
 
